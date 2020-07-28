1ef79864faac3a5c51a2859d7066acd661b98b19
aleontiev <alonetiev@gmail.com>
2020-07-28 11:16:37 -0700

    wip

---
+++ tests/unit/preql/test_build.py
@@ -0,0 +1,69 @@
+from adbc.preql import build
+from adbc.preql.dialect import Dialect, Backend, ParameterStyle
+
+
+def get_dialect():
+    return Dialect(
+        backend=Backend.POSTGRES, style=ParameterStyle.FORMAT
+    )
+
+def test_build_create():
+    dialect = get_dialect()
+    expectations = [
+        ( # 1. create one database
+            {
+                'create': {
+                    'database': 'test'
+                }
+            },
+            [('CREATE DATABASE "test"', [])]
+        ),
+        ( # 2. create two schemas
+            {
+                'create': [{
+                    'schema': 'one',
+                }, {
+                    'schema': 'two'
+                }]
+            },
+            [
+                ('CREATE SCHEMA "one"', []),
+                ('CREATE SCHEMA "two"', [])
+            ]
+        ),
+        (
+            {
+                'create': {
+                    'table': {
+                        'name': 'one.test',
+                        'columns': [{
+                            'name': 'id',
+                            'type': 'int',
+                            'null': False,
+                        }, {
+                            'name': 'name',
+                            'type': 'text',
+                            'null': True
+                        }],
+                        'constraints': [{
+                            'type': 'p',
+                            'columns': ['id'],
+                            'name': 'test__id__pk',
+                        }],
+                    }
+                }
+            },
+            [
+                (
+                    'CREATE TABLE "one"."test" (\n'
+                    '    "id" int NOT NULL,\n'
+                    '    "name" text,\n'
+                    '    CONSTRAINT "test__id__pk" PRIMARY KEY ("id") NOT DEFERRABLE INITIALLY IMMEDIATE\n'
+                    ')', ()
+                )
+            ]
+        )
+    ]
+
+    for query, result in expectations:
+        assert build(query, dialect=dialect) == result
